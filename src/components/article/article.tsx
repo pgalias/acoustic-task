e795b80be8f434b785c8ed6596f3041829e50f07
Paweł Galias <pawgalias@gmail.com>
2020-02-05 18:31:54 +0100

    styles: rearrange article item component appearance

--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -1,8 +1,7 @@
 import React, { FunctionComponent } from 'react';
-import { Container, Grid } from '@material-ui/core';
+import { Button, Container, Grid, Divider } from '@material-ui/core';
 import { hasEnoughPropsToRender } from '../../utils/helpers/article.helper';
 import { Article as ArticleModel } from '../../models/article';
-import Image from '../shared/image';
 import Text from '../shared/text';
 import FormattedText from '../shared/formattedText';
 import Date from '../shared/date';
@@ -20,59 +19,57 @@ const Article: FunctionComponent<Props> = (props: Props) => {
   }
 
   return (
-    <div data-testid="article" className={styles.article}>
-      <div className={styles.heading}>
-        {mainImage?.leadImage?.src ? (
-          <Image
-            src={mainImage.leadImage.src}
-            alt={mainImage.leadImage.alt}
-            imageClassName={styles.image}
-            containerClassName={styles.imageContainer}
-          />
-        ) : (
-          <Image
-            src="http://placekitten.com/270/"
-            alt="kitten"
-            imageClassName={styles.image}
-            containerClassName={styles.imageContainer}
-          />
-        )}
-        {heading && (
-          <div className={styles.ribbon}>
-            <Container>
-              <Grid container spacing={1} alignItems="center">
-                <Grid item xs={12} data-testid="heading">
-                  <h4 className={styles.header}>{heading}</h4>
-                </Grid>
-              </Grid>
-            </Container>
-          </div>
-        )}
-      </div>
+    <div
+      data-testid="article"
+      className={styles.article}
+      style={{ backgroundImage: `url(${mainImage?.leadImage?.src})` }}
+    >
+      <Container>
+        <Grid container alignItems="center">
+          {heading && (
+            <Grid item xs={12}>
+              <h4 className={styles.header}>{heading}</h4>
+            </Grid>
+          )}
 
-      {(body?.length > 0 || author || date) && (
-        <div className={styles.content}>
-          <Container>
-            <Grid container spacing={1} alignItems="center">
-              {author && (
-                <Grid item xs data-testid="author">
-                  <small>Author:</small>
-                  <Text className={styles.text}>{author}</Text>
-                </Grid>
-              )}
-              {date && (
-                <Grid item xs className={styles.date} data-testid="date">
-                  <small>Posted:</small>
-                  <Date date={date} locale={'pl-Pl'} className={styles.text} />
-                </Grid>
-              )}
-              <Grid item xs={12} className={styles.contentBody}>
-                <FormattedText texts={body} limit={150} />
+          {(author || date) && (
+            <Grid item xs={12}>
+              <Grid container alignItems="center" justify="space-between">
+                {author && (
+                  <Grid item xs>
+                    <small>Author:</small>
+                    <Text className={styles.text}>{author}</Text>
+                  </Grid>
+                )}
+                {date && (
+                  <Grid item xs className={styles.date}>
+                    <small>Posted:</small>
+                    <Date
+                      date={date}
+                      locale={'pl-Pl'}
+                      className={styles.text}
+                    />
+                  </Grid>
+                )}
               </Grid>
             </Grid>
-          </Container>
-        </div>
-      )}
+          )}
+
+          {body?.length > 0 && (
+            <Grid item xs={12} className={styles.contentBody}>
+              <FormattedText texts={body} limit={150} />
+            </Grid>
+          )}
+        </Grid>
+
+        <Divider variant="middle" light className={styles.divider} />
+
+        <Grid container justify="flex-start" className={styles.buttonContainer}>
+          <Button variant="contained" color="primary">
+            See more
+          </Button>
+        </Grid>
+      </Container>
     </div>
   );
 };
